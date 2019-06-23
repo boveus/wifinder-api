02420c4d720e229668939cf84fed7b05a105a096
Brandon Stewart <boveus@gmail.com>
2019-06-23 13:13:02 -0400

    fix people migration

--- db/migrate/20190623163138_create_people.rb
+++ db/migrate/20190623163138_create_people.rb
@@ -1,8 +1,8 @@
 class CreatePeople < ActiveRecord::Migration[5.2]
   def change
     create_table :people do |t|
-      t.string nickname
-      
+      t.string :nickname
+
       t.timestamps
     end
   end
