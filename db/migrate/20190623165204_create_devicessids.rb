8d85590bca27d7386923686f23b81d5e4fbd9861
Brandon Stewart <boveus@gmail.com>
2019-06-23 12:58:52 -0400

    correct typo in devicessids migration

--- db/migrate/20190623165204_create_devicessids.rb
+++ db/migrate/20190623165204_create_devicessids.rb
@@ -1,4 +1,4 @@
-class CreateDevicessids < ActiveRecord::Migration[5.2]
+class CreateDevicesssids < ActiveRecord::Migration[5.2]
   def change
     create_table :devicessids do |t|
       t.belongs_to :device, index: true
