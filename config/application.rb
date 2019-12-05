f1b0e5aded3748a8d3fa966e35b8c985e9abd088
Brandon Stewart <boveus@gmail.com>
2019-12-04 21:21:52 -0500

    fix cors

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,15 @@ module WifinderApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # ALLOW ALL ORIGINS
+    # TODO make this secure
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
